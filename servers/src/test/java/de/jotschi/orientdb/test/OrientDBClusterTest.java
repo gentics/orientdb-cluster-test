d72a110fd1a7c2f0a839337c98739515872f4718
Johannes Schüth <j.schueth@gentics.com>
2017-08-01 12:39:46 +0200

    Add vertx event handling code

--- servers/src/test/java/de/jotschi/orientdb/test/OrientDBClusterTest.java
+++ servers/src/test/java/de/jotschi/orientdb/test/OrientDBClusterTest.java
@@ -6,7 +6,6 @@ import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.orientechnologies.orient.core.Orient;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 
@@ -18,14 +17,11 @@ public class OrientDBClusterTest extends AbstractClusterTest {
 	@Before
 	public void cleanup() throws Exception {
 		FileUtils.deleteDirectory(new File("target/data1"));
-		FileUtils.deleteDirectory(new File("target/data2"));
 		initDB(nodeName, basePath);
 	}
 
 	@Test
 	public void testCluster() throws Exception {
-		Orient.instance().startup();
-
 		// 1. Setup the plocal database
 		db.setupPool();
 
@@ -33,13 +29,15 @@ public class OrientDBClusterTest extends AbstractClusterTest {
 		db.addVertexType("Product", null);
 
 		// 3. Now start the OServer and provide the database to other nodes
+		startVertx();
 		db.startOrientServer();
 
 		// Now continue to insert some nodes in the database
 		while (true) {
 			OrientGraph tx = db.getTx();
+			vertx.eventBus().publish("dummy", "hello world");
 			try {
-				Vertex v = tx.addVertex("Product");
+				Vertex v = tx.addVertex("class:Product");
 				v.setProperty("name", "SOME VALUE");
 				System.out.println("Count: " + db.getNoTx().countVertices());
 				Thread.sleep(500);
--- servers/src/test/java/de/jotschi/orientdb/test/OrientDBClusterTest2.java
+++ servers/src/test/java/de/jotschi/orientdb/test/OrientDBClusterTest2.java
@@ -1,9 +1,11 @@
 package de.jotschi.orientdb.test;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.orientechnologies.orient.core.Orient;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 
@@ -14,14 +16,14 @@ public class OrientDBClusterTest2 extends AbstractClusterTest {
 
 	@Before
 	public void setup() throws Exception {
+		FileUtils.deleteDirectory(new File("target/data2"));
 		initDB(nodeName, basePath);
 	}
 
 	@Test
 	public void testCluster() throws Exception {
-		Orient.instance().startup();
-
 		// 1. Start the orient server - it will connect to other nodes and replicate the found database
+		startVertx();
 		db.startOrientServer();
 
 		// 2. Replication may occur directly or we need to wait.
@@ -30,11 +32,14 @@ public class OrientDBClusterTest2 extends AbstractClusterTest {
 		// 3. The db has now been replicated. Lets open the db
 		db.setupPool();
 
+		vertx.eventBus().consumer("dummy", rh -> {
+			System.out.println("Received message: " + rh.body());
+		});
 		// 4. Insert some vertices
 		while (true) {
 			OrientGraph tx = db.getTx();
 			try {
-				Vertex v = db.getNoTx().addVertex("Product");
+				Vertex v = tx.addVertex("class:Product");
 				v.setProperty("name", "SOME VALUE");
 				System.out.println("Count: " + tx.countVertices());
 				Thread.sleep(1500);
