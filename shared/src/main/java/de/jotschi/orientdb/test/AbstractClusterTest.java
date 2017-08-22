c930734698ecff5bf2466112327b942a9f24fb51
Johannes Schüth <git@jotschi.de>
2017-08-22 20:48:30 +0200

    Reduce scope of the example

--- shared/src/main/java/de/jotschi/orientdb/test/AbstractClusterTest.java
+++ shared/src/main/java/de/jotschi/orientdb/test/AbstractClusterTest.java
@@ -1,57 +1,11 @@
 package de.jotschi.orientdb.test;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.node.Node;
-import org.elasticsearch.node.NodeBuilder;
-
-import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
-
 public class AbstractClusterTest {
 
 	protected Database db;
 
-	protected Vertx vertx;
-
 	public void initDB(String name, String graphDbBasePath) throws Exception {
 		db = new Database(name, graphDbBasePath);
-	}
-
-	public void startVertx() throws InterruptedException {
-		VertxOptions options = new VertxOptions();
-		options.setClustered(true);
-		CountDownLatch latch = new CountDownLatch(1);
-		Vertx.clusteredVertx(options, rh -> {
-			System.out.println("Vertx Joined Cluster");
-			vertx = rh.result();
-			latch.countDown();
-		});
-		latch.await(10, TimeUnit.SECONDS);
-	}
-
-	public Node startESNode(String nodeName) {
-		Settings settings = Settings.settingsBuilder()
-
-				.put("threadpool.index.queue_size", -1)
-
-				.put("http.enabled", false)
-
-				.put("node.name", nodeName)
-
-				.put("node.local", false)
-
-				.put("http.cors.enabled", "true")
-
-				.put("path.data", "essearch").build();
-		NodeBuilder builder = NodeBuilder.nodeBuilder();
-		// TODO configure ES cluster options
-		System.out.println("Starting ES");
-		Node node = builder.clusterName("testESCluster").settings(settings).node();
-		// node.start();
-		return node;
 	}
 
 }
